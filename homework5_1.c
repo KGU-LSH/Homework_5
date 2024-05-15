392e8b3a849f4adbbe28314f8925b875afd0b9ad
KGU-LSH <163758468+KGU-LSH@users.noreply.github.com>
2024-05-15 13:42:48 +0900

    Week 5 Example Upload

--- homework5_1.c
+++ homework5_1.c
@@ -1,7 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 int main()
 {
 	srand(time(NULL));
